89327b7102b4b43d7db985174a5d606f2306d424
William Delaosa <wdelaosa24@gmail.com>
2025-10-30 16:41:02 -0400

    connecting redis and backend together in a seperate branch

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
-import express  from 'express'
+import express from 'express'
 import redis from './redis.js'
+
 const app = express()
 const port = 3000
 
-app.listen (async () => {
-    await redis.connect();
-})
+// Connect to Redis before starting the server
+await redis.connect();
+console.log('Connected to Redis');
 
 app.get('/', (req, res) => {
     res.send('Hi Perla')
-  })
+})
   
 app.listen(port, () => {
-    console.log(`Example app listening on port ${3000}`)
+    console.log(`Example app listening on port ${port}`)
 })
