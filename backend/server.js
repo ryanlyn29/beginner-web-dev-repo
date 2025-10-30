a6e8e1b976467385ed0212b1d3580e874e89b611
David Bucio-Paz <daviddbp.buciopaz@gmail.com>
2025-10-30 16:39:53 -0400

    redis database connected

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 import express  from 'express'
 import redis from './redis.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
 const app = express()
 const port = 3000
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const signInHTMLPath = path.join(__dirname, '../src/auth', 'sign-in.html');
+app.use(express.json());
 
 app.listen (async () => {
     await redis.connect();
 })
 
 app.get('/', (req, res) => {
-    res.send('Hi Perla')
+    res.sendFile(signInHTMLPath);
   })
   
 app.listen(port, () => {
