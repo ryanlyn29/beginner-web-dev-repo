4bea83f920b1f2d0d9d04fdd8ef5432d6f445d94
perlancc <pcaceres2005@gmail.com>
2025-11-12 09:23:37 -0500

    got signin/login working with redis

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,19 @@ import express  from 'express'
 import redis from './redis.js'
 import path from 'path';
 import { fileURLToPath } from 'url';
+
 const app = express()
 const port = 3000
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const signInHTMLPath = path.join(__dirname, "../src/auth", "sign-in.html");
-const mainAppPath = path.join(__dirname, "../src", "mainapp.html");
+const signInHTMLPath = path.join(__dirname, "../src/Pages", "signin.html");
+const mainAppPath = path.join(__dirname, "../src/Pages", "homepage.html");
+const logInHTMLPath = path.join(__dirname, "../src/Pages", "login.html");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../src')));
 
 redis.connect().catch(console.error);
 
@@ -19,34 +22,62 @@ app.get("/", (req, res) => {
   res.sendFile(signInHTMLPath);
 });
 
+app.get("/signin", (req, res) => {
+    res.sendFile(signInHTMLPath);
+});
 //this is where the users will be entering their info to sign up and the data will be sent over to the 
 //database
-app.post("/signin", (req, res) => {
-  const { password } = req.body;
-  const storedPassword = "hello";
+app.post("/signin", async (req, res) => {
+    const { name, email, password } = req.body;
+    console.log('recieved data', req.body);
 
-  if (password == storedPassword) {
-    res.redirect("/mainpage");
-  } else {
-    res.status(401).send("Invalid username or password");
-  }
+    try {
+        const userId = await redis.get(`email:${email}`);
+        if (userId) {
+            res.status(400).send("E-mail already registered. Try logging in.");
+        }
 
+        const uniqueId = Date.now().toString();
+
+        await redis.hSet(`user:${uniqueId}`, {
+            id: String(uniqueId),
+            name: String(name),
+            email: String(email),
+            password: String(password)
+        });
+    
+        await redis.set(`email:${email}`, uniqueId);
+    
+        res.redirect('/mainpage');
+        
+    } catch(error) {
+        console.error('Full error details:', error);
+        console.error('Error message:', error.message);
+        res.status(500).send('Error creating account');
+      
+    }
+});
+
+
+app.get("/login", (req, res) => {
+    res.sendFile(logInHTMLPath);
 });
 
 //this is here as placeholder for when we have the login form page that will 
 //check information that users enter  
-app.post("/login", (req, res) => {
-  const { password } = req.body;
-  const storedPassword = "hello";
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
 
-  if (password == storedPassword) {
+    const userId = await redis.get(`email:${email}`);
+
+    const user = await redis.hGetAll(`user:${userId}`);
+
+    if (user.password !== password){
+        return res.status(401).send("Invalid credentials.")
+    }
     res.redirect("/mainpage");
-  } else {
-    res.status(401).send("Invalid username or password");
-  }
 
 });
-
 
 //this is where users will be and it will be where they can enter the room code
 //to begin the implementation of socket.io and join a room.
